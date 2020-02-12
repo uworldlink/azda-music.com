eead3b4ccb4ac2aa995772f0340b03cd731576a0
uworldlink <59925342+uworldlink@users.noreply.github.com>
2020-02-12 16:34:23 +0700

    Create theme-selector.js

---
+++ components/theme/theme-selector.js
@@ -0,0 +1,57 @@
+themeSelect(current_theme);
+function themeSelect(state) {
+	var auto_icon = "url(https://uploads-ssl.webflow.com/5cb8748fb2c690ee31a27874/5e2eef7b0421c294957c8b27_auto-mode-01.svg)";
+	var light_icon = "url(https://uploads-ssl.webflow.com/5cb8748fb2c690ee31a27874/5e25d924bfb39b331cd06007_sun.svg)";
+	var dark_icon = "url(https://uploads-ssl.webflow.com/5cb8748fb2c690ee31a27874/5e25d995e8d47e509e9e452e_moon.svg)";
+	var current = document.getElementById("current-mode");
+	var mode_1 = document.getElementById("theme-1");
+	var mode_2 = document.getElementById("theme-2");
+	
+  if ((state === null) || (state == "system")) {
+    current.style.backgroundImage=auto_icon;
+    mode_1.style.backgroundImage=light_icon;
+    mode_2.style.backgroundImage=dark_icon;
+    $('.theme-text-1').text("Light");
+    $('.theme-text-2').text("Dark");
+    $('.theme-1-mode').attr("whenClick", "setMode('light')");
+    $('.theme-2-mode').attr("whenClick", "setMode('dark')");
+  } 
+  else if (state == "light") {
+    current.style.backgroundImage=light_icon;
+    mode_1.style.backgroundImage=dark_icon;
+    mode_2.style.backgroundImage=auto_icon;
+    $('.theme-text-1').text("Dark");
+    $('.theme-text-2').text("Auto");
+    $('.theme-1-mode').attr("whenClick", "setMode('dark')");
+    $('.theme-2-mode').attr("whenClick", "setMode('system')");
+  }
+  else if (state == "dark") {
+    current.style.backgroundImage=dark_icon;
+    mode_1.style.backgroundImage=light_icon;
+    mode_2.style.backgroundImage=auto_icon;
+    $('.theme-text-1').text("Light");
+    $('.theme-text-2').text("Auto");
+    $('.theme-1-mode').attr("whenClick", "setMode('light')");
+    $('.theme-2-mode').attr("whenClick", "setMode('system')");
+  }
+}
+
+$(document).ready(function() {
+	var anchors = document.getElementsByTagName('*');
+	for(var i = 0; i < anchors.length; i++) {
+		var anchor = anchors[i];
+		anchor.onclick = function() {
+			code = this.getAttribute('whenClick');
+			eval(code);
+		}
+	}
+})
+									
+function setMode(mode){
+	var isStorageEnabled = ! (typeof localStorage == 'undefined');
+	if(isStorageEnabled){
+		localStorage.setItem('theme', mode);
+	}
+	theme_detect(mode);
+	themeSelect(mode);
+}
